e9312c8ae3ba1ebbb94e2ef9664b92c982dd38ba
thcorreia <43270885+thcorreia@users.noreply.github.com>
2022-10-15 09:55:10 +0200

    ramdom numbers

--- project.js
+++ project.js
@@ -1,3 +1,31 @@
+function getRandomArray() {
+    var numbers = []; // new empty array
+
+    var min, max, length, randomNumber, isDuplicated;
+    
+    min = 1;
+    max = 9;
+    length = 9; // how many numbers you want to extract
+    
+    for (let number = 0; number < length; number++) {
+      do {
+        // get a random number inside the min and max
+        randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        // check if the random number is already in the list
+        isDuplicated = numbers.includes(randomNumber);
+        // if not in the list, then add it
+        if(!isDuplicated){
+          numbers.push(randomNumber);
+        }
+      }
+      // keep doing the loop until the random number is not duplicated
+      while(isDuplicated);
+    }
+
+    // return the list
+    return numbers;
+}
+
 // Show errors on page
 var errorsElement = document.querySelector("#errors");
 var totalErrors = 0;
@@ -9,13 +37,15 @@ var grid = [];
 for (var box = 1; box < 10; box++) {
     // Cels for each box
     var cels = [];
+    var randomNumbers = getRandomArray();
+
     for (var cel = 1; cel < 10; cel++) { 
         var celValue = {
             id: cel,
-            value: cel,
+            value: randomNumbers[cel - 1],
             element: document.querySelector("#box-" + box + " .cel-" + cel + ' input')
         };
-        celValue.element.value = cel;
+        celValue.element.value = celValue.value;
         cels.push(celValue);
     }
 
@@ -27,3 +57,7 @@ for (var box = 1; box < 10; box++) {
 }
 
 console.log(grid)
+
+
+
+console.log(getRandomArray());
