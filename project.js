824f0a0d1781f7bf8baef53399ec52e086f1bc0c
thcorreia <43270885+thcorreia@users.noreply.github.com>
2022-10-31 11:34:06 +0100

    functions, added comments

--- project.js
+++ project.js
@@ -1,32 +1,33 @@
-// Methods Declaration
 function getRandomArray() {
-    var numbers = []; // new empty array
+  // New empty array
+  var numbers = []; 
 
-    var min, max, length, randomNumber, isDuplicated;
-        
-    min = 1;
-    max = 9;
-    length = 9; // how many numbers you want to extract
-    
-    for (let number = 0; number < length; number++) {
-      do {
-        // get a random number inside the min and max
-        randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-        // check if the random number is already in the list
-        isDuplicated = numbers.includes(randomNumber);
-        // if not in the list, then add it
-        if(!isDuplicated){
-          numbers.push(randomNumber);
-        }
+  var min, max, length, randomNumber, isDuplicated;
+  
+  // Limits of the random numbers
+  min = 1;
+  max = 9;
+  // How many numbers you want to extract
+  length = 9; 
+  
+  for (let number = 0; number < length; number++) {
+    do {
+      // Get a random number inside the min and max
+      randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      // Check if the random number is already in the list
+      isDuplicated = numbers.includes(randomNumber);
+      // If not in the list, then add it
+      if(!isDuplicated){
+        numbers.push(randomNumber);
       }
-      // keep doing the loop until the random number is not duplicated
-      while(isDuplicated);
     }
+    // Keep doing the loop until the random number is not duplicated
+    while(isDuplicated);
+  }
 
-    // return the list
-    return numbers;
+  // Return the list
+  return numbers;
 }
-
 
 function getRandomNumber(randomNumbers, cel, box) {
   if (box > 1) {
@@ -36,7 +37,6 @@ function getRandomNumber(randomNumbers, cel, box) {
         var min = 0;
         var max = 4;
         var cels = currentBox.cels.filter(boxCel => boxCel.id > min && boxCel.id < max).map(boxCel => boxCel.value);
-        
       }
     }
     return randomNumbers[0];
@@ -45,77 +45,97 @@ function getRandomNumber(randomNumbers, cel, box) {
 }
 
 function addError() {
+  // Increment the errors counter
   totalErrors++;
+  // Update the html element with the new value
   document.getElementById("errors").innerHTML = totalErrors;
 }
 
-// method that receives: celNumber, inputNumber and htmlElement. 
-// This method will check if the two numbers are equal, 
-// if so, set the value to the html element, 
-// if not, call addError
 function checkNumber(inputNumber, htmlElement) {
-  // get the data-value attribute from the html element
+  // Get the data-value attribute from the html element
   var celNumber = htmlElement.getAttribute("data-value");
-
+  
+  // If inputNumber is not empty or null
   if (!!inputNumber) {
+    // Check if the two numbers are equal
     if (celNumber == inputNumber) {
+      // if so, set the value to the html element, and add the class "correct" to the html element
       htmlElement.innerHTML = celNumber;
       htmlElement.disabled = true;
       htmlElement.classList.remove("bg-danger");
       htmlElement.classList.add("bg-success");
     } else {
+      // if not, call addError method and add the class "error" to the html element
       htmlElement.classList.remove("bg-success");
       htmlElement.classList.add("bg-danger");
       addError();
     }
   } else {
+    // If the input is empty, remove the class "correct" and "error" from the html element
     htmlElement.classList.remove("bg-danger");
     htmlElement.classList.remove("bg-success");
   }
 }
 
+function initCel(cel, box, randomNumber) {
+  // Creates the new cel object with cel id number, random value and the html input element
+  var celValue = {
+      id: cel,
+      value: randomNumber,
+      element: document.querySelector("#box-" + box + " .cel-" + cel + ' input')
+  };
+  // add data-value attribute to the html element to save the random value in the element
+  celValue.element.setAttribute("data-value", randomNumber);
+
+  // Watch for input changes on each cel and call checkNumber, passing the input value and the html target element
+  celValue.element.addEventListener('input', function(event) {
+    checkNumber(event.data, event.target);
+  }, false);
+
+  // return the new cel object to the caller
+  return celValue;
+}
+
+function initBox(box) {
+  // List of cels for each box
+  var cels = [];
+  // List of random number to be used on each box
+  var randomNumbers = getRandomArray();
+
+  // Loop for each cel...
+  for (var cel = 1; cel < 10; cel++) { 
+    // Get random number from the list to the cel
+    var randomNumber = getRandomNumber(randomNumbers, cel, box);
+    // Remove the number from the random list
+    randomNumbers = randomNumbers.filter(random => random !== randomNumber);
+    // Add the cel to the box's cels list
+    var newCel = initCel(cel, box, randomNumber);
+    cels.push(newCel);
+  }
+
+  return {
+      id: box,
+      cels: cels
+  };
+}
+
+function initGrid() {
+  // Loop for each box...
+  for (var box = 1; box < 10; box++) {
+    var newBox = initBox(box);
+    // Add box to the grid list
+    grid.push(newBox);
+  }
+}
 
 // Show errors on page
 var totalErrors = 0;
 
-
-
-// Generate cels
+// List of boxes
 var grid = [];
 
-for (var box = 1; box < 10; box++) {
-    // Cels for each box
-    var cels = [];
-    var randomNumbers = getRandomArray();
-
-    for (var cel = 1; cel < 10; cel++) { 
-      var randomNumber = getRandomNumber(randomNumbers, cel, box);
-      randomNumbers = randomNumbers.filter(random => random !== randomNumber);
-      
-      var celValue = {
-          id: cel,
-          value: randomNumber,
-          element: document.querySelector("#box-" + box + " .cel-" + cel + ' input')
-      };
-
-  // add html data-value property to the html element
-   celValue.element.setAttribute("data-value", randomNumber);
-
-
-   // Watch for input changes on each cel and call checkNumber
-   celValue.element.addEventListener('input', function(event) {
-     checkNumber(event.data, event.target);
-   }, false);
-   cels.push(celValue);
- }      
- 
-    // each box
-    grid.push({
-        id: box,
-        cels: cels
-    });
-}
+// Start the board
+initGrid();
 
 console.log(grid)
-
 console.log(getRandomArray());
