0be0bedefc77270482aaa58cd654b61567daa629
thcorreia <43270885+thcorreia@users.noreply.github.com>
2022-11-16 14:23:30 +0100

    Final Version (With comments)

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# Sudoku
- 
-# Sudoku
+## Sudoku Game 
+This is a project that creates a popular Japanese puzzle game called Sudoku. It is based on the logical placement of numbers.
+How to play: The goal of Sudoku is to fill in a 9×9 grid with digits so that each column, row, and 3×3 section contain the numbers between 1 to 9. At the beginning of the game, the 9×9 grid will have some of the squares filled in. Your job is to use logic to fill in the missing digits and complete the grid. We also offer the grids 2x2 and one single grid to play. 
+
+## Tools for development 
+Vanilla JavaScript, HTML and CSS
+Visual Studio Code - Visual Studio Code is used to develop the code
+
+## How to Install and Run the Project
+Use git clone or fork or download as a zip file and exctract in your machine. Navigate to the extracted folder and double click in the index.HTML file. It will open the game in the browser.
+
+## Technical Implementation
+
+The code builds the game by looping through the variables that set the columns and rows for the grid, based on the user selection in the UI buttons (3x3, 2x2, 1x1). 
+
+The method that creates the Sudoku solution for the random numbers in the grid, will follow the dimentions from the user selection, and runs in loop N times until the solution is valid or until the maximum looping count is exceeded. Some solutions will not be valid depending on the first line of random numbers. After the fourth row some solutions will not have available numbers to fill the cels, because some combinations of lines, rows and boxes will not have unique values that satisfied the Sudoku conditions, setting undefinied value to the cel. After each solution is calculated in the trial loop a function checks if the solutions contains undefinied values, if the solution contains only numbers and not undefinied values than the solutions is valid and the create grid method is called.
+
+The grid is built by creating the boxes and then the list of cels for each box. WHen the cel is created in javaScript, the div and the input for the HTML are created and fed with the random number possible for that grid position, coming from the solution list from the last step. The input element receives at the moment of creation a callback function that checks if the user typed number is the same as the random number associated to that cel. In case of being the same number the input will be disable and a sucess green background is added to the cel. In negative case the cel will be painted with red and the error counter on the grid's header will be incremented. Also in the creation moment the cel will receive from the filters board list, a random mask solution to hide and show the random values in the cell, so we can have the intial Sudoku state with some values on the board. 
+
+
--- project.js
+++ project.js
@@ -1,3 +1,5 @@
+//works as a filter - true and false. 0=false, 1=true. We run the game and randomly we get one of these boards. 
+//Where is 0= false we don't show the number and where is 1 = the number is showed
 const FILTER_BOARDS = [
   [
     [0, 1, 0, 0, 0, 0, 0, 0, 1],
@@ -36,27 +38,35 @@ const FILTER_BOARDS = [
 
 // Show errors on page
 var totalErrors = 0;
-var filterId = getFilterRandomNumber();
+//Get a random N. Get the Filter_Boards. Return 0 - 2 bc there are 3 boards
+var filterId = getFilterRandomNumber(); 
+//Random value list. This is the result. This variable will contain the random N that solves the Grid
 var solution = [];
 
 // List of html element boxes
 var grid = [];
+//variables with the HTML elements manipulated by the game
 var gridElement = document.getElementById("grid");
 var calculatingElement = document.getElementById("calculating");
 var calculatingTryElement = document.getElementById("calculating-counter");
 var errorElement = document.getElementById("error");
 var boardSizeElement = document.getElementById("board-size");
+//Game variables. How many columns of boxes the game will have, how many rows of boxes the game will have
 var gridBoxesCols = null;
 var gridBoxesRows = null;
+//definition of rows and columns by box
 var boxCols = 3;
 var boxRows = 3;
+//how many times it caculates possible solutions without success (solutions that have undefinied numbers)
 var calculatingTry = 0;
 
+//It takes the possible filter to hide the board solution (FILTER_BOARDS has three filters, the random number must be 0-2)
 function getFilterRandomNumber() {
   // Get a random number between 0 and 2
   return Math.floor(Math.random() * 2);
 }
 
+//returns a list of random numbers from 0 - 9 to fill each grid line
 function getRandomArray() {
   // New empty array
   var numbers = []; 
@@ -69,10 +79,11 @@ function getRandomArray() {
   // How many numbers you want to extract
   length = 9; 
   
+  // loop through 0-9 checking if the random number generated is already in the number list, if not than add to the list
   for (let number = 0; number < length; number++) {
     do {
       // Get a random number inside the min and max
-      randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      randomNumber = Math.floor(Math.random() * (max - min + 1)) + min; 
       // Check if the random number is already in the list
       isDuplicated = numbers.includes(randomNumber);
       // If not in the list, then add it
@@ -88,6 +99,7 @@ function getRandomArray() {
   return numbers;
 }
 
+//updates the screen when a new error happens with the total errors
 function addError() {
   // Increment the errors counter
   totalErrors++;
@@ -95,6 +107,7 @@ function addError() {
   document.getElementById("errors").innerHTML = totalErrors;
 }
 
+//the function will check if the typed number by the user is the same as the solution in the HTML element, if yes - correct if not error
 function checkNumber(inputNumber, htmlElement) {
   // Get the data-value attribute from the html element
   var celNumber = htmlElement.getAttribute("data-value");
@@ -121,60 +134,83 @@ function checkNumber(inputNumber, htmlElement) {
   }
 }
 
+//creates a HTML element with a Box to add to the grid. (Creates the box)
 function createBoxElement() {
+  //creates a new div 
   var box = document.createElement("div");
+  //add class box to the div
   box.classList.add("box");
+  //add 2 styles : width and height in % based in the number of N of gridboxes and N of grid rows
   box.setAttribute("style", "width: " + (100 / gridBoxesCols) + "%; height: " + (100 / gridBoxesRows) + "%;");
   return box;
 }
 
+//creates a HTML element with Cel to add to the grid
 function createCelElement(box, cel, randomNumber) {
+  //creates a new div 
   var celElement = document.createElement("div");
+  //add class cel to the div
   celElement.classList.add("cel");
+  //add the HTML element for the input passing the box and cel position and the random value for the input
   celElement.append(createInputElement(box, cel, randomNumber));
   return celElement;
 }
 
+//the randomNumber is the value for the solution in the position Box x Cel
 function createInputElement(box, cel, randomNumber) {
   var input = document.createElement("input");
   input.setAttribute("type", "number");
+  //attribute the randomNumber for the solution into the input element for checking purposes
   input.setAttribute("data-value", randomNumber);
+  //Get a random filter based on filterId, box and cel positions and check if should show the value on the board (compares ===1)
   input.value = FILTER_BOARDS[filterId][box - 1][cel - 1] === 1 ? randomNumber : "";
+  //add a listener to the inputs events, when the user types a new value in this input, then check the input number by the user against the solution randomNumber
   input.addEventListener("input", function (event) {
     checkNumber(event.data, event.target);
   }, false);
   return input;
 }
 
+// creates a logic cel for the logic box in the grid list 
 function initCel(cel, box, randomNumber, gridElement) {
-  // Creates the new cel object with cel id number, random value and the html input element
+  // Creates the new cel object with cel id number, random solution value and the html input element
   var celValue = {
     id: cel,
     value: randomNumber,
     element: createCelElement(box, cel, randomNumber),
   };
 
+  //Pushes the new HTML cel element to the grid HTML element
   gridElement.append(celValue.element)
   // return the new cel object to the caller
   return celValue;
 }
 
+// creates a logic Box in the grid list
 function initBox(box) {
   // List of cels for each box
   var cels = [];
   var boxElement = createBoxElement(box);
   
-  // Loop for each cel...
+  // Loop for each cel, while cel is < boxCols * boxRows =  (3x3/9) add + 1  
   for (var cel = 1; cel < ((boxCols * boxRows) + 1); cel++) {
+    //calculates the row position of the cel inside the box, divide the number of the cel by the number of the columns. We subtract 1 from the box number to convert to the array counting
+    //after dividing we remove the decimals, so we have the row number
     var boxRow = Math.floor((box - 1) / gridBoxesCols);
+    //the column number will be the module of the box number -1 (conversion to array logic) by the number of columns
     var boxCol = (box - 1) % gridBoxesCols;
+    //calculates the row in the grid based on the box position in the grid,example box 1,2,3 - first row = 0 / box 4,5,6 - first row = 2 and etc
     var row = Math.floor((cel - 1) / boxCols) + (boxRow * boxRows);
+     //calculates the col in the grid based on the box position in the grid,example box 1,4,7 - first col = 0 / box 2,5,8 - first col = 2 and etc
     var col = (cel - 1) % boxCols + (boxCol * boxCols); 
+    //with the row and col from the grid we can extract the random number from the solution grid
     var value = solution[row][col];
+    //create a new logic cel
     var newCel = initCel(cel, box, value, boxElement);
+    //push the new cel logic to the list of cels in the box
     cels.push(newCel);
   }
-
+//add new HTML box element to the screen 
   gridElement.append(boxElement);
 
   return {
@@ -183,6 +219,7 @@ function initBox(box) {
   };
 }
 
+// first function to create the logic board and the inside boxes
 function initBoard() {
   // Loop for each box...
   for (var box = 1; box < ((gridBoxesCols * gridBoxesRows) + 1); box++) {
@@ -193,12 +230,15 @@ function initBoard() {
   }
 }
 
+//find all the numbers for this row number to check if the new number is not duplicated in this Sudoku line
 function getCurrentRowNumbers(rowNumber) {
   return solution[rowNumber] || [];
 }
 
+//find all the numbers for this column number to check if the new number is not duplicated in this Sudoku column
 function getCurrentColumnNumbers(columnNumber) {
   var column = [];
+  //loop through all the previous lines and get only the numbers for the same column
   for (var row = 0; row < solution.length; row++) {
     var currentColumn = solution[row][columnNumber];
     
@@ -209,10 +249,12 @@ function getCurrentColumnNumbers(columnNumber) {
   return column;
 }
 
+//receives a list of random numbers from the list of the row or column and returning only the numbers inside the box
 function filterBoxList(list, start) {
   return [...list].filter((number, index) => index >= start && index < start + boxRows);
 }
 
+//find all the numbers already in the same box, a funcao no caso junta os numeros ja preeenchidos de colunas e linhas dentro do box 
 function getCurrentBoxNumbers(rowNumber, columnNumber) {
   var rowOffset = Math.floor(rowNumber / boxRows);
   var colOffset = Math.floor(columnNumber / boxCols); // offset from box to box in the grid
@@ -221,12 +263,12 @@ function getCurrentBoxNumbers(rowNumber, columnNumber) {
   var rowOne = Math.floor(boxNumber / gridBoxesCols) * boxCols;
   var boxColumnNumbers = [];
   var boxRowNumbers = [];
-
+  //check the columns for the existing numbers
   for (var i = 0; i < boxCols; i++) {
     var currentCol = getCurrentColumnNumbers(columnOne + i);
     boxColumnNumbers.push(...filterBoxList(currentCol, rowOne));
   }
-
+  //check the rows for the existing numbers
   for (var i = 0; i < boxRows; i++) {
     var currentRow = getCurrentRowNumbers(rowOne + i);
     boxRowNumbers.push(...filterBoxList(currentRow, columnOne));
@@ -235,14 +277,14 @@ function getCurrentBoxNumbers(rowNumber, columnNumber) {
   return [...boxColumnNumbers, ...boxRowNumbers];
 }
 
+//loop through rowns and columns and try to fit the random numbers to the grid without repeting the random numbers in the line, column and box
 function populateGrid() {
   solution = [];
-  var aTable = solution;
   // loop for rows/lines
   for (var row = 0; row < (gridBoxesRows * boxRows); row++) {
     solution.push([]);
     var rowList = solution[row];
-    // list of randomm numbers for the columns
+    // list of randomm numbers for the row
     var randomList = getRandomArray();
 
     // loop for columns
@@ -260,22 +302,30 @@ function populateGrid() {
         var currentColumnNumbers = getCurrentColumnNumbers(col);
         // get the number from the previous box
         var currentBoxNumbers = getCurrentBoxNumbers(row, col);
+        //join all the numbers from the columns, rowns and the boy in one list of numbers already taken
         var currentNumbers = [...currentRowNumbers, ...currentColumnNumbers, ...currentBoxNumbers];
+        //by creating a new set with the current numbers we remove the duplicated numbers
         var takenNumbers = new Set(currentNumbers);
+        //available numbers by filtering the taken numbers from the available random numbers list
         var availableNumbers = randomList.filter(number => !takenNumbers.has(number));
+        //take the first number from the available numbers list to use as a random number for the cel
+        //depending on the combinaiton columns, rows and boxes some cels will not have a random number available bc all the possible combinations are taken
         var randomNumber = availableNumbers.pop();
+        //remove from the available random Numbers list after using the first number in this cell
         randomList = randomList.filter(number => number !== randomNumber);
         rowList.push(randomNumber);
-        console.log('test ', solution);
       }
     }
   }
 }
 
+//loop through the rows to check if there is any undefinied random value in any cel of the grid, if underfined value is found the grid is not a valid Sudoku solution
 function validateGrid() {
   for(var i = 0; i < (gridBoxesRows * boxRows); i++) {
     var row = solution[i];
+    //create a list of the row values
     var rowSet = new Set(row);
+    //check if the row contains undefinied value
     if (rowSet.has(undefined)) {
       return false;
     }
@@ -283,15 +333,19 @@ function validateGrid() {
   return true;
 }
 
+//method called when the user clicks in the options of grid game
 function initGame(cols, rows) {
+  //the grid dimentions is calculated based on these 2 variables
    gridBoxesCols = cols;
    gridBoxesRows = rows;
+   //the game start with the solution "isValid" as false 
    var isValid = false;
+   //show in the screen that the game is calculating the board and hide the buttons for the grid options 
    calculatingElement.removeAttribute("hidden");
    boardSizeElement.setAttribute("hidden", true);
-
+  //to give the screen time enough to changethe elements above before start calculating the solution, bc when the solution starts calculating the browser gets freeze
    setTimeout(function () {
-
+    //arbitrary number of temptives, a bigger number can take more time, but has higher chances of sucess
     for(var i = 0; i < 1000; i++) {
       calculatingTryElement.innerHTML = i;
 
@@ -299,15 +353,14 @@ function initGame(cols, rows) {
       isValid = validateGrid();
       
       if(isValid) {
-        console.log('sucesso ', i, solution);
         initBoard();
         break;
       }
     }
     if (!isValid) {
-        console.log('erro ', solution);
         errorElement.removeAttribute("hidden");
     }
+    //100 mileseconds to starts calculating so the screen has time to update the loading message
     calculatingElement.setAttribute("hidden", true);
     }, 100);
 }
